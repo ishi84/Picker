0b1069f237a773f310b1da3b3c61ea6862375619
大石海 <ishibasi48@gmail.com>
2020-02-10 21:23:06 +0900

    カメラ操作の実装
    スクリプト作成中

--- Assets/MainCameraControll.cs
+++ Assets/MainCameraControll.cs
@@ -1,44 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class MainCameraControll : MonoBehaviour {
 
 	[SerializeField]
-	private Transform pivot = null;
+	private GameObject playerChar;
+	private float difference;
 
 	void Start() {
-		if (pivot == null)
-			pivot = transform.parent;
+		this.difference = playerChar.transform.position.z - this.transform.position.z;
 	}
-	[Range(-0.999f, -0.5f)]
-	public float minYAngle = -0.5f;
-	[Range(0.5f, 0.999f)]
-	public float maxYAngle = 0.5f;
 
 	void Update() {
-		//マウスのX,Y軸がどれほど移動したかを取得します
-		float X_Rotation = Input.GetAxis("Mouse X");
-		float Y_Rotation = Input.GetAxis("Mouse Y");
-		//Y軸を更新します（キャラクターを回転）取得したX軸の変更をキャラクターのY軸に反映します
-		pivot.transform.Rotate(0, X_Rotation, 0);
-
-		//次はY軸の設定です。
-		float nowAngle = pivot.transform.localRotation.x;
-		//最大値、または最小値を超えた場合、カメラをそれ以上動かない用にしています。
-		//カメラが一回転しないようにするのを防ぎます。
-		if (-Y_Rotation != 0) {
-			if (0 < Y_Rotation) {
-				if (minYAngle <= nowAngle) {
-					pivot.transform.Rotate(Y_Rotation, 0, 0);
-				}
-			} else {
-				if (nowAngle <= maxYAngle) {
-					pivot.transform.Rotate(Y_Rotation, 0, 0);
-				}
-			}
-		}
-		//操作していると、Z軸がだんだん動いていくので、0に設定してください。
-		pivot.eulerAngles = new Vector3(pivot.eulerAngles.x, pivot.eulerAngles.y, 0f);
+		float X_Rotation = CrossPlatformInputManager.GetAxis("CamX");
+		float Y_Rotation = CrossPlatformInputManager.GetAxis("CamY");
 	}
 }
