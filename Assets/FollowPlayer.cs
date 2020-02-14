13003f2f402cbec1c0c8bfd4dd2eb6e0f369a4ec
大石海 <ishibasi48@gmail.com>
2020-02-14 21:22:10 +0900

    カメラ操作の実装
    MainGameのカメラ操作スクリプト作成中

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private GameObject target;
+	private Transform targetPos;
+
+	[Range(0, 2)][SerializeField]
+	float offsetY = 1.5f;
+
+	// Use this for initialization
+	void Start() {
+		target = GameObject.Find("PlayerChar");
+		targetPos = target.transform;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		this.transform.position = new Vector3(targetPos.position.x, targetPos.position.y + offsetY, targetPos.position.z);
+	}
+}
--- Assets/MainCameraControll.cs
+++ Assets/MainCameraControll.cs
@@ -1,20 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.EventSystems;
 
 public class MainCameraControll : MonoBehaviour {
 
-	[SerializeField]
-	private GameObject playerChar;
-	private float difference;
+	//回転感度
+	private float senseX = 0.1f;
+	private float senseY = 0.04f;
+	//親要素の位置(キャラに追従)
+	private Transform targetPos;
+	//垂直方向に回転できる上限角度
+	private float maxAngle = 2.5f;
+	//垂直方向に回転できる下限角度
+	private float minAngle = 0.5f;
 
 	void Start() {
-		this.difference = playerChar.transform.position.z - this.transform.position.z;
+		//親要素の位置を取得
+		targetPos = transform.parent.gameObject.transform;
 	}
 
 	void Update() {
-		float X_Rotation = CrossPlatformInputManager.GetAxis("CamX");
-		float Y_Rotation = CrossPlatformInputManager.GetAxis("CamY");
+		if (Input.touchCount > 0) {
+			foreach (Touch touch in Input.touches) {
+				//UIがタップされていた場合は除外
+				if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId)) {
+					// タッチの移動量(横)
+					float deltaX = touch.deltaPosition.x;
+					// タッチの移動量(縦) 上下反転
+					float deltaY = touch.deltaPosition.y * -1;
+
+					// targetPosを中心に横回転
+					transform.RotateAround(targetPos.position, Vector3.up, deltaX * senseX);
+
+					//maxAngleとminAngleの範囲内でのみ動作
+					if ((transform.position.y < maxAngle && deltaY > 0) || (transform.position.y > minAngle && deltaY < 0)) {
+						// targetPosを中心に縦回転
+						transform.RotateAround(targetPos.position, transform.right, deltaY * senseY);
+					}
+				}
+			}
+		}
 	}
 }
