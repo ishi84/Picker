36d432dc72a86754bc19e751db3a55a60376d0b0
大石海 <ishibasi48@gmail.com>
2020-02-21 21:18:31 +0900

    MAPデータ実装、WebGL用操作を追加

--- Assets/MainCameraControll.cs
+++ Assets/MainCameraControll.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class MainCameraControll : MonoBehaviour {
 
-	//回転感度
-	[SerializeField] private float senseX = 0.2f;
-	[SerializeField] private float senseY = 0.08f;
 	//親要素の位置(キャラに追従)
 	private Transform targetPos;
 	//垂直方向に回転できる上限角度
-	private float maxAngle = 2.5f;
+	private float maxAngle = 3.0f;
 	//垂直方向に回転できる下限角度
 	private float minAngle = 0.5f;
 
@@ -18,6 +15,11 @@ public class MainCameraControll : MonoBehaviour {
 		//親要素の位置を取得
 		targetPos = transform.parent.gameObject.transform;
 	}
+
+#if UNITY_IOS
+	//iOSビルド時のカメラ回転感度
+	private float senseX = 0.2f;
+	private float senseY = 0.08f;
 
 	public void CameraDrag() {
 		foreach (Touch touch in Input.touches) {
@@ -36,4 +38,25 @@ public class MainCameraControll : MonoBehaviour {
 			}
 		}
 	}
+#endif
+
+#if UNITY_WEBGL
+	//WebGLビルド時のカメラ回転感度
+	private float senseX = 2.0f;
+	private float senseY = 2.0f;
+
+	void Update(){
+		//マウスの移動量
+		float deltaX = Input.GetAxis("Mouse X");
+		float deltaY = Input.GetAxis("Mouse Y") * -1;
+		// targetPosを中心に横回転
+		transform.RotateAround(targetPos.position, Vector3.up, deltaX * senseX);
+
+		//maxAngleとminAngleの範囲内でのみ動作
+		if ((transform.position.y < maxAngle && deltaY > 0) || (transform.position.y > minAngle && deltaY < 0)) {
+			// targetPosを中心に縦回転
+			transform.RotateAround(targetPos.position, transform.right, deltaY * senseY);
+		}
+	}
+#endif
 }
---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	[SerializeField] private GameObject wallObj;
+	[SerializeField] private GameObject itemObj;
+	[SerializeField] private GameObject doorObj;
+	private int[, ] mapData;
+	//1マスの大きさ
+	private int cellSize = 4;
+
+	// Use this for initialization
+	void Start() {
+		mapData = new int[21, 27] {
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+			{0,1,0,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,1,0},
+			{0,1,0,2,0,1,0,2,0,1,1,1,1,1,1,1,1,1,0,2,0,1,0,2,0,1,0},
+			{0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0},
+			{0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+			{0,1,0,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,1,0},
+			{0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0},
+			{0,0,0,0,0,0,0,1,0,1,0,0,0,3,0,0,0,1,0,1,0,0,0,0,0,0,0},
+			{0,1,1,1,1,1,1,1,0,1,0,2,2,2,2,2,0,1,0,1,1,1,1,1,1,1,0},
+			{0,1,0,0,0,0,0,1,1,1,0,2,2,2,2,2,0,1,1,1,0,0,0,0,0,1,0},
+			{0,1,1,1,1,1,1,1,0,1,0,2,2,2,2,2,0,1,0,1,1,1,1,1,1,1,0},
+			{0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0},
+			{0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+			{0,1,0,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,1,0},
+			{0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0},
+			{0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0},
+			{0,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,0},
+			{0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0},
+			{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		};
+		MapGenerate(mapData.GetLength(0), mapData.GetLength(1));
+	}
+
+	private void MapGenerate(int x, int z) {
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < z; j++) {
+				switch (mapData[i, j]) {
+					case 0:
+						Instantiate(wallObj, new Vector3(i * cellSize, wallObj.transform.position.y, j * cellSize), Quaternion.identity);
+						break;
+					case 1:
+						Instantiate(itemObj, new Vector3(i * cellSize, itemObj.transform.position.y, j * cellSize), Quaternion.identity);
+						break;
+					case 2:
+						break;
+					case 3:
+						Instantiate(doorObj, new Vector3(i * cellSize, doorObj.transform.position.y, j * cellSize), Quaternion.identity);
+						break;
+				}
+			}
+		}
+	}
+}
